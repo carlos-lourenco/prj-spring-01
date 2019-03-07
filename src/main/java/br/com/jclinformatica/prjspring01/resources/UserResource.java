cb4ccfa80f22e4c345715b018d7ebc0055321323
carlos lourenço <carlos.lourenco.work@gmail.com>
2019-03-07 00:00:10 -0300

    Get user by id

--- src/main/java/br/com/jclinformatica/prjspring01/resources/UserResource.java
+++ src/main/java/br/com/jclinformatica/prjspring01/resources/UserResource.java
@@ -2,15 +2,18 @@ package br.com.jclinformatica.prjspring01.resources;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.List;import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.jclinformatica.prjspring01.domain.User;
+import br.com.jclinformatica.prjspring01.dto.UserDTO;
 import br.com.jclinformatica.prjspring01.services.UserService;
 
 @RestController
@@ -21,8 +24,18 @@ public class UserResource {
 	private UserService service;
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
+	
+	@RequestMapping(value="/{id}",method=RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(user));
+	}
+	
+
+
 }
--- src/main/java/br/com/jclinformatica/prjspring01/services/UserService.java
+++ src/main/java/br/com/jclinformatica/prjspring01/services/UserService.java
@@ -7,16 +7,21 @@ import org.springframework.stereotype.Service;
 
 import br.com.jclinformatica.prjspring01.domain.User;
 import br.com.jclinformatica.prjspring01.repository.UserRepository;
+import br.com.jclinformatica.prjspring01.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 	
 	@Autowired
-	private UserRepository repo;
+	private UserRepository userRepository;
 	
 	public List<User> findAll() {
-		return repo.findAll();
+		return userRepository.findAll();
 		
 	}
-
+	
+	public User findById(String id) throws ObjectNotFoundException {
+		return userRepository.findById(id)
+				.orElseThrow(()->new ObjectNotFoundException("Usuário não encontrado"));		
+	}
 }
